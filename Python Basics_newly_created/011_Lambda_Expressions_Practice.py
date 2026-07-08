a87206b6d6b39322330e4774ee270ce7982ed9e7
shashank-automation-qa <officialsinghshashank091@gmail.com>
2026-07-08 22:48:02 +0530

    Lambda expression

--- Python Basics_newly_created/011_Lambda_Expressions_Practice.py
+++ Python Basics_newly_created/011_Lambda_Expressions_Practice.py
@@ -36,3 +36,69 @@ print(square_cube(5))
 #With Lambda Expression check if the given statement is palindrome or not
 palindrome= lambda statement: "palindrome" if statement==statement[::-1] else "not palindrome"
 print(palindrome("dad"))
+
+#*****************************************    Map()  ****************************************************
+
+#Syntax:   map(function_to_apply, list_of_inputs)
+
+# write a program to check if the numbers are even or odd in the given list using map
+l = list(range(10))
+even_odd = lambda num: "even" if num % 2 == 0 else "odd"
+res = map(even_odd, l)
+print(list(res))
+
+# write a program to check if the string is palindrome or not
+l = ["madam", "dad", "hello", "google", "level"]
+map_palindrome = lambda word: "palindrome" if word==word[::-1] else "not palindrome"
+res = map(map_palindrome, l)
+print(list(res))
+
+# write a program to create a list of square numbers of the
+# numbers in the list
+l = [1, 2, 3, 4, 5]
+_square=lambda i: i**2
+print(list(map(_square, l)))
+
+#to convert the strings to upper case
+list_ = ["madam", "dad", "hello", "google", "level"]
+upper_=lambda word:word.upper()
+print(list(map(upper_,list_)))
+#           OR
+print(list(map(str.upper,list_)))
+
+# to swap case of the words in the given sentence
+sentence = "This IS a BunCh of WORDS"
+l=sentence.split()
+print(list(map(str.swapcase,l)))
+
+# to convert the negative numbers into positive in the list
+numbers = [-9, 2, 5, -4, 8, -2, 7, -12]
+
+
+# to return the pair of word and its length in the sentence
+sentence = "This IS a BunCh of WORDS"
+
+#*****************************************    Filter()  ****************************************************
+
+#Syntax:   filter(function_to_apply, list_of_inputs)
+
+#WAP to extract only even numbers from the given list
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+def even_(num):
+    if num % 2 == 0:
+        return num ** 2
+print(list(map(even_, numbers)))      # ==>>  [None, 4, None, 16, None, 36, None, 64]
+print(list(filter(even_, numbers)))   # ==>>  [2, 4, 6, 8]
+
+# extract the names which have length > 4
+names = ["greg", "steve", "bob", "alexa"]
+
+
+# Build a list with only even length strings using filter class
+names = ['apple', 'google', 'yahoo', 'facebook', 'yelp', 'flipkart', 'gmail', 'instagram']
+
+# Return the string if the string is starting with a vowel character"
+names = ['laura', 'steve', 'bill', 'james', 'bob', 'greig', 'scott', 'alex', 'ive']
+
+# Program to return only positive values in the list using filter class
+numbers = [-2, -1, 0, 1, 2]
