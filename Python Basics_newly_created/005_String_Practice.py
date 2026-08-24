bc8c6b4ad6c9d4ab2f3d61952c00b3ca2079a2bb
shashank-automation-qa <officialsinghshashank091@gmail.com>
2026-08-24 17:14:18 +0530

    String questions code from dhina

--- Python Basics_newly_created/005_String_Practice.py
+++ Python Basics_newly_created/005_String_Practice.py
@@ -192,3 +192,90 @@ print(res)
 s="Shashank"
 for i in reversed(s):
     print(i, end='')
+
+# WAP to validate below scenario
+'''
+test_password_1 = "Python2026"  # Should pass
+test_password_2 = "python 20"   # Should fail (has space, no uppercase)
+test_password_3 = "PYTHONCODE"  # Should fail (no lowercase, no number)
+'''
+
+password =  "python2026"
+small_alpha_count=0
+capital_alpha_count=0
+num_count=0
+
+if " " in password:
+    print("Space not allowed in password")
+else:
+    for i in password:
+        if ord('a')<=ord(i)<=ord('z'):
+            small_alpha_count+=1
+        elif ord('A')<=ord(i)<=ord('Z'):
+            capital_alpha_count+=1
+        elif i in "0123456789":
+            num_count+=1
+    else:
+        if small_alpha_count >0  and capital_alpha_count >0 and num_count >0:
+            print("pass is correct")
+        else:
+            print("include capital letter, small letter and numbers in your password")
+
+# WAP to validate below scenario
+'''
+user_input_1 = "+1 (555) 123.4567 "
+user_input_2 = "555-123-ABCD"
+'''
+phone = "+1 (555) 123.4567 "
+phone = phone[::-1]
+correct_number=""
+count=0
+for i in phone:
+    if i.isdigit():
+        correct_number = i + correct_number
+        count+=1
+        if count>9:
+            break
+    elif i.isupper() or i.islower():
+        print("incorrect phone number")
+        break
+print(correct_number)
+
+#Wap to validate below scenario
+server_logs = """INFO: 2026-08-24 - System booted up normally.
+CRITICAL: 2026-08-24 - Database connection lost.
+WARNING: 2026-08-24 - High memory usage detected.
+CRITICAL: 2026-08-24 - Payment Gateway timeout, database is locked.
+INFO: 2026-08-24 - User admin logged in."""
+
+data_base_count=0
+list_server_logs = server_logs.split("\n")
+for msg in list_server_logs:
+    if "CRITICAL:" in msg:
+        list_msg= msg.lower().split()
+        data_base_count+=list_msg.count("database")
+print(data_base_count)
+
+# Practice Challenge 6: "The Receipt Printer"
+# You are building the software for a coffee shop register. You need to print a receipt that
+# is exactly 30 characters wide.
+# The title must be perfectly centered with = padding.
+# The item name must align to the left.
+# The price must align to the right, and the price needs to be padded with zeros (e.g., $04.50).
+
+# Goal Output:
+# Plaintext
+# =========== RECEIPT ==========
+# Cappuccino              $04.50
+
+# receipt_width = 30
+# title = " RECEIPT "
+# item = "Cappuccino"
+# price = "4.5"  # Needs to become $04.50
+
+Name= input("Enter purchase item name")
+Price= input("Enter purchase item price")
+total_item_len= len(Name)+len(Price)
+space_len = 30-total_item_len
+space_len = " "*(space_len-1)
+print(f"=========== RECEIPT ==========\n{Name}{space_len}${Price}")
